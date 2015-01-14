b90848807717a027024f94322d174741f41082f2
Boris Sirota <borisirota@gmail.com>
2015-01-14 15:55:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+**Install**
+
+    `npm install jsdoc2md-anchors --save`
+
+**Usage**
+
+    This utility outputs the github format by default. Its possible to pass 'bitbucket' as parameter in order to output bitbucket's format:
+    `jsdoc2md-anchors [bitbucket]`
+
+**Example**
+
+    `jsdoc2md lib/**/*.js -t readme.hbs | jsdoc2md-anchors bitbucket > README.md`
---
+++ bin/jsdoc2md-anchors.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+'use strict';
+
+var replace = require('replacestream');
+var anchor = require('anchor-markdown-header');
+var SERVICES = ['github', 'bitbucket'];
+var service = SERVICES[0];
+
+if ((process.argv.length >= 2) && SERVICES.indexOf(process.argv[2]) !== -1) {
+    service = process.argv[2];
+}
+
+if (service === SERVICES[0]) {
+
+    process.stdin
+        //make the inner links (anchors) inside the readme to be compatible with bitbucket
+        .pipe(replace(/\[(.+)\]\((#.+)\)/g, function (match) {
+            return anchor(match[1], 'github');
+        }))
+        .pipe(process.stdout);
+
+} else {
+
+    process.stdin
+        //remove all local anchors
+        .pipe(replace(/\n<a name=.+><\/a>/g, ''))
+        //adjust to bitbucket's markdown levels
+        .pipe(replace(/(  +)([*\-])/g, function (match) {
+            return new Array(match[1].length + 1).join('  ') + match[2];
+        }))
+        //make the inner links (anchors) inside the readme to be compatible with bitbucket
+        .pipe(replace(/\[(.+)\]\((#.+)\)/g, function (match) {
+            return anchor(match[1], 'bitbucket.org');
+        }))
+        .pipe(process.stdout);
+
+}
